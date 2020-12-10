7536e9d70e9682397d03121840344859a1dcd3a0
Carter Tune <cartertune@gmail.com>
2020-12-10 14:14:39 -0600

    position & mobile view

--- src/UI/home_page/homePage.js
+++ src/UI/home_page/homePage.js
@@ -7,20 +7,24 @@ const HomePage = (props) => {
   return (
     <div className="home-page">
       <div className="row h-75">
-        <div className="col-8 title-container">
+        <div className="col-6 col-lg-8 title-container">
           <CarterTuneText />
         </div>
-        <div className="col-4 links-container">
+        <div className="col links-container">
           <H2 clickable>Projects</H2>
           <H2 clickable>Books</H2>
           <H2 clickable>Blog</H2>
           <H2 clickable>About</H2>
         </div>
       </div>
-      <div className="row hardwood-floor">
-        <div className="col-12 h-100">
-          <div className="carter-image-container">
-            <img src={carter}></img>
+      <div className="hardwood-floor">
+        <div className="container">
+          <div className="row h-100">
+            <div className="col-12 h-100">
+              <div className="carter-image-container">
+                <img src={carter}></img>
+              </div>
+            </div>
           </div>
         </div>
       </div>
